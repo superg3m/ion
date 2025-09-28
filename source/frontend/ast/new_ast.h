c59534dae933cc466ea6546f77d8c27301f9937e
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-09-27 20:23:20 -0400

    Create new_ast.h

---
+++ source/frontend/ast/new_ast.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "../lexer/token.h"
+
+// |00000|000 00000000 00000000 00000000
+// bit 27: LEAF_NODE
+// bit 28: DEFERRABLE
+// bit 29: EPXRESSION
+// bit 30: STATEMENT
+// bit 31: DECLARATION
+
+#define ION_LEAF_NODE_BIT ((unsigned int)(1 << 27))
+#define ION_DEFERRABLE_BIT ((unsigned int)(1 << 28))
+#define ION_EXPRESSION_BIT ((unsigned int)(1 << 29))
+#define ION_STATEMENT_BIT ((unsigned int)(1 << 30))
+#define ION_DECLARATION_BIT ((unsigned int)(1 << 31))
+
+typedef enum NodeKind {
+    ION_NK_UNARY_EXPR = ION_EXPRESSION_BIT,
+    ION_NK_BINARY_EXPR,
+    ION_NK_GROUPING,
+
+    // LEAF NODES
+    ION_NK_INTEGER_EXPR = ION_LEAF_NODE_BIT|ION_EXPRESSION_BIT,
+    ION_NK_FLOAT_EXPR,
+    ION_NK_BOOLEAN_EXPR,
+    ION_NK_STRING_EXPR,
+    ION_NK_IDENTIFIER_EXPR,
+
+
+    ION_NK_ASSIGNMENT_STMT = ION_STATEMENT_BIT,
+    ION_NK_BLOCK_STMT,
+    ION_NK_PRINT_STMT = ION_DEFERRABLE_BIT|(ION_NK_BLOCK_STMT + 1),
+    ION_NK_PRINTLN_STMT,
+
+    ION_NK_POST_INCREMENT_SE = ION_EXPRESSION_BIT|ION_STATEMENT_BIT, // i++
+    ION_NK_PRE_INCREMENT_SE = ION_EXPRESSION_BIT|ION_STATEMENT_BIT,  // ++i
+    ION_NK_POST_DECREMENT_SE = ION_EXPRESSION_BIT|ION_STATEMENT_BIT, // i--
+    ION_NK_PRE_DECREMENT_SE = ION_EXPRESSION_BIT|ION_STATEMENT_BIT,  // --i
+    ION_NK_FUNC_CALL_SE = ION_DEFERRABLE_BIT|ION_EXPRESSION_BIT|ION_STATEMENT_BIT,
+
+
+    ION_NK_VAR_DECL = ION_DECLARATION_BIT,
+    ION_NK_FUNC_DECL,
+} IonNodeKind;
+
+typedef struct IonNode {
+    IonNodeKind kind;
+    IonToken token; // return, binary: operator, int, float, string, bool, identifer
+    union {
+        u32 desc_count;
+
+        // if leaf
+        int i;
+        float f;
+        bool b;
+        CKG_StringView s;
+    } data;
+} IonNode;
+
+/*
+// OP is stored in the token
+
+SHUNTING YARD: 5 2 7 * +
+push this 
+Binary(*, left=2, right=7)
+*/
+
+IonNode ionNodeCreate(IonNodeKind kind, IonToken token) {
+    IonNode ret;
+    ret.kind = kind;
+    ret.token = token;
+    ret.data = {0};
+
+    return ret;
+}
+
+bool ionNodeIsLeaf(IonNode node) {
+    return node.kind & ION_DECLARATION_BIT;
+}
+
+bool ionNodeIsDeclaration(IonNode node) {
+    return node.kind & ION_DECLARATION_BIT;
+}
+
+bool ionNodeIsStatement(IonNode node) {
+    return node.kind & ION_STATEMENT_BIT;
+}
+
+bool ionNodeIsExpression(IonNode node) {
+    return node.kind & ION_EXPRESSION_BIT;
+}
