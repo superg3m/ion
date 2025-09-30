3686304507ccd2bd754fb67f7ea08015ba8c80f5
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-09-30 15:00:51 -0400

    removed bs

--- source/Frontend/lexer/lexer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <ckg.h>
-#include "token.h"
-
-CKG_Vector(IonToken) ionLexerGenerateTokenStream(u8* source, u64 source_length);
--- source/Frontend/parser/parser.h
+++
@@ -1,3 +0,0 @@
-#include "../ast/ast.h"
-
-CKG_Vector(IonNode) ionParseProgram(CKG_Vector(IonToken) tokens);
